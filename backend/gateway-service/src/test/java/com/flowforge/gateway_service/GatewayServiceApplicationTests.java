40e061c5333759c35baa632bbfe0f0f3e99cc55d
Ajay Bambhaniya <ajaybambhaniya9898@gmail.com>
2026-07-26 15:07:39 +0530

    feat(auth): setup PostgreSQL configuration for auth service

---
+++ backend/gateway-service/src/test/java/com/flowforge/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowforge.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
